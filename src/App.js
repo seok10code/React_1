8d3465e70d0ff12cac263f197664bff4a8e45fcf
Seokwon <73082542+seok10code@users.noreply.github.com>
2021-10-13 11:29:03 +0900

    10/13 I learned how to add, remove, toggle in array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,72 @@
-import "./styles.css";
-import InputSample1 from "./Counter.js";
-// import Counter from "./Counter.js"
+import React, { useRef, useState } from "react";
+import CreateUser from "./CreateUser";
+import UserList2 from "./UserList2.js";
 
 function App() {
-  return <InputSample1 />;
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: ""
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com"
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com"
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com"
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
+
+    setInputs({
+      username: "",
+      email: ""
+    });
+
+    nextId.current += 1;
+  };
+
+  const onRemove = (id) => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList2 users={users} />
+    </>
+  );
 }
 
 export default App;
